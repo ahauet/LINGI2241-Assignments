cbb6078fce193ecf26eaa29c78a2a5b0be05908e
Kagelmacher <alexandre.hauet@gmail.com>
2015-10-15 12:31:46 +0200

    new version of task1_1

--- Assigment1/task1_1/Task1_1.java
+++ Assigment1/task1_1/Task1_1.java
@@ -1,6 +1,14 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 
 public class Task1_1 {
@@ -20,27 +28,145 @@ public class Task1_1 {
 		//Cache size
 		int n = Integer.parseInt(args[1]);
 		
+		int nbProcessedRequest = 0;
+		
 		LRUCache LRU = new LRUCache(n);
-		int size = 0;
+		LFUCache LFU = new LFUCache(n);
+		
 		String s;
 		while ((s = in.readLine()) != null && s.length() != 0) {
-			String tmp[] = s.split(" ");
-			LRU.add(tmp[0]);
-			size++;
+			if (x == 0) {
+				String request[] = s.split(" ");
+				LRU.add(request[0]);
+				LFU.add(request[0]);
+				nbProcessedRequest++;
+			} else {
+				x--;
+			}
 		}
-		System.out.println("Size = "+ size);
-		LRU.printHitMiss();
-		
-		//String[] list = {"A","B", "C", "D", "A", "B", "E", "A", "B", "C" ,"D" ,"E"};
-		//String[] list = {"A","A", "A", "D", "A", "B", "E", "A", "B", "C" ,"D" ,"E"};
+		if(nbProcessedRequest > 0) {
+			System.out.println("LRU Hit rate : " + LRU.getHit()+"%");
+			System.out.println("LFU Hit rate : " + LFU.getHit() +"%");
+		} else {
+			System.out.println("Nothing write in the cache");
+		}
 
-		//LRUCache LRU = new LRUCache(n);
+		LRU.writeInFile();
+		LFU.writeInFile();
 		
-		
-		/*for(String s: list) {
-			LFU.add(s);
-			LFU.print();
-		}*/
+	}
+}
+
+class LRUCache {
+	
+	private int cacheSize;
+	private LinkedList<String> list;
+	private int miss = 0;
+	private int hit = 0;
+	
+	public LRUCache(int cacheSize) {
+		this.list = new LinkedList<String>();
+		this.cacheSize = cacheSize;
+	}
+	
+	public void add (String s) {
+		if (list.contains(s)) {
+			list.remove(s);
+			list.add(s);
+			hit++;
+		}
+		else if(list.size() == cacheSize) {
+			list.pop();
+			list.add(s);
+			miss++;
+		} else {
+			list.add(s);
+			miss++;
+		}
+	}
+	
+	public int getHit() {
+		return this.hit;
+	}
+	
+	public void print() {
+		System.out.println(Arrays.toString(list.toArray()));
+	}
+	
+	public void printHitMiss() {
+		System.out.println("Hit = " +hit+" Miss = " +miss);
+	}
+	
+	public void writeInFile() throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("cache_lru.txt", "UTF-8");
+		Iterator<String> it = list.iterator();
+		while(it.hasNext()) {
+			writer.println(it.next());
+		}
+		writer.close();
+	}
+	
+}
+
+class LFUCache {
+	private int cacheSize;
+	private Map<String, Integer> list;
+	private int miss = 0;
+	private int hit = 0;
+	
+	public LFUCache(int cacheSize) {
+		this.list = new LinkedHashMap<String, Integer>();
+		this.cacheSize = cacheSize;
+	}
+	
+	public int getHit() {
+		return this.hit;
+	}
+
+	public void add (String s) {
+		if (list.containsKey(s)) {
+			int frequency = list.remove(s);
+			list.put(s, frequency + 1);
+			hit++;
+		} else if (list.size() == cacheSize) {
+			Iterator<String> it = list.keySet().iterator();
+			String tmpKey = "";
+			int tmpFrequency = Integer.MAX_VALUE;
+			while(it.hasNext()) {
+				String key = it.next();
+				int frequency = list.get(key);
+				if (frequency < tmpFrequency) {
+					tmpKey = key;
+					tmpFrequency = frequency;
+				}
+			}
+			list.remove(tmpKey);
+			list.put(s,1);
+			miss++;
+		} else {
+			list.put(s, 1);
+			miss++;
+		}
+	}
+	
+	public void print() {
+		Iterator<String> it = list.keySet().iterator();
+		System.out.print("[ ");
+		while(it.hasNext()) {
+			String key = it.next();
+			int frequency = list.get(key);
+			System.out.print("{ " + key + ":" + frequency + " }");
+		}
+		System.out.println(" ]");
+	}
+	
+	public void writeInFile() throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("cache_lfu.txt", "UTF-8");
+		Iterator<String> it = list.keySet().iterator();
+		while(it.hasNext()) {
+			writer.println(it.next());
+		}
+		writer.close();
 	}
 }
 
