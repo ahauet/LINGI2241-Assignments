bed3d9369e45586d145b912fe747f4fc4f7e06a3
Kagelmacher <alexandre.hauet@gmail.com>
2015-12-19 12:05:29 +0100

    add new picture and read args

--- Assigment2/task1/Client.java
+++ Assigment2/task1/Client.java
@@ -13,8 +13,33 @@ import javax.imageio.ImageIO;
 
 public class Client {
 	static int bytesRead;
-	
-	public static void ClientRequest(String nameFile) throws Exception {
+	// Each file names in this array correspond to a problem 
+	// For example : 	file name at index 0 is the problem number 1
+	//								file name at index x is the problem number x+1
+	private static String[] files = {"picture50x32.jpg","picture75x48.jpg", 
+			"picture150x96.jpg", "picture250x160.jpg", "picture400x300.jpg",
+			"picture640x409.jpg", "picture800x511.jpg", "picture1024x645.jpg", "picture1600x1022.jpg"};
+	// Name for the solution of a problem
+	private static String[] result_files = {"picture50x32_result.jpg","picture75x48_result.jpg", 
+			"picture150x96_result.jpg", "picture250x160_result.jpg", "picture400x300_result.jpg",
+			"picture640x409_result.jpg", "picture800x511_result.jpg", "picture1024x645_result.jpg", "picture1600x1022_result.jpg"};
+
+	public static void main(String[] args) throws Exception {
+		
+		// Check the usage, we need one argument => the problem number
+		if(args.length != 1) {
+			System.err.println("Usage : java client problem_number");
+			System.exit(-1);
+		}
+		
+		int problem_number = Integer.parseInt(args[0]);
+		
+		//Check if the problem exist
+		if (problem_number < 1 || problem_number > files.length) {
+			System.err.println("Error : the problem number must be between 1 and " + files.length);
+			System.exit(-1);
+		}
+		
 		
 		Socket socket = new Socket("localhost", 13085);
 
@@ -22,7 +47,7 @@ public class Client {
 
 		OutputStream outputStream = socket.getOutputStream();
 		// Read the image from the file
-		BufferedImage image = ImageIO.read(new File(nameFile));
+		BufferedImage image = ImageIO.read(new File(files[problem_number-1]));
 		// Create the byteArrayOutputStream
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		// Convert BufferedImage to byteArrayOutputStream
@@ -57,23 +82,16 @@ public class Client {
 				sizeReaded += bytesRead;
 			}
 		} 
-		System.out.println(sizeReaded);
 
 		
 		BufferedImage outputImage = ImageIO.read(new ByteArrayInputStream(imageAr));
-		ImageIO.write(outputImage, "jpg", new File(nameFile+"_received2.jpg"));
+		ImageIO.write(outputImage, "jpg", new File(result_files[problem_number-1]));
 		System.out.println("close socket");
 
 
 
 		socket.close();
-		
-	}
 
-	public static void main(String[] args) throws Exception {
-		ClientRequest("picture2.jpg");
-		ClientRequest("picture1.jpg");
-		
 	}
 
 }
--- Assigment2/task1/Server.java
+++ Assigment2/task1/Server.java
@@ -12,92 +12,78 @@ import java.nio.ByteBuffer;
 import javax.imageio.ImageIO;
 
 public class Server {
-	
-	
 
 	private static int bytesRead;
 	static BufferedImage outputImage;
 
 	public static void main(String[] args) throws Exception {
-		
-		ServerSocket serverSocket = null;
-		Socket socket = null;
-		
+
+		ServerSocket serverSocket = new ServerSocket(13085);
+		Socket socket = serverSocket.accept();
+		InputStream inputStream = socket.getInputStream();
+
+		System.out.println("Reading: " + System.currentTimeMillis());
+		// size of the in byte[] of the file received
+		byte[] sizeAr = new byte[4];
+		// read the size
+		inputStream.read(sizeAr);
+		// convert the size in byte[] to int
+		int sizeIn = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
+		// Image in bite[]
+		byte[] imageAr = new byte[sizeIn];
+		// size of the image already read
+		int sizeReaded = 0;
+		// while the image is not completely read
+		while(bytesRead > -1 && sizeReaded != sizeIn){
+			// read the image
+			bytesRead = inputStream.read(imageAr, sizeReaded, sizeIn-sizeReaded);
+
+			if(bytesRead >= 0){
+				sizeReaded += bytesRead;
+			}
+		} 
+
+
+		// change the image in color to black and white
 		try {
-			serverSocket = new ServerSocket(13085,1);
-			while (true) {
-				System.out.println("Waiting...");
-				try {
-					socket = serverSocket.accept();
-					InputStream inputStream = socket.getInputStream();
 
-					System.out.println("Reading: " + System.currentTimeMillis());
-					// size of the in byte[] of the file received
-					byte[] sizeAr = new byte[4];
-					// read the size
-					inputStream.read(sizeAr);
-					// convert the size in byte[] to int
-					int sizeIn = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
-					// Image in bite[]
-					byte[] imageAr = new byte[sizeIn];
-					// size of the image already read
-					int sizeReaded = 0;
-					// while the image is not completely read
-					while(bytesRead > -1 && sizeReaded != sizeIn){
-						// read the image
-						bytesRead = inputStream.read(imageAr, sizeReaded, sizeIn-sizeReaded);
-
-						if(bytesRead >= 0){
-							sizeReaded += bytesRead;
-						}
-					} 
-
-
-					// change the image in color to black and white
-					try {
-
-						BufferedImage inputImage = ImageIO.read(new ByteArrayInputStream(imageAr));
-						outputImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(),BufferedImage.TYPE_INT_RGB);
-						for (int x = 0; x < inputImage.getWidth(); x++) {
-							for (int y = 0; y < inputImage.getHeight(); y++) {
-								int rgb = inputImage.getRGB(x, y);
-								int blue = 0x0000ff & rgb;
-								int green = 0x0000ff & (rgb >> 8);
-								int red = 0x0000ff & (rgb >> 16);
-								int lum = (int) (red * 0.299 + green * 0.587 + blue * 0.114);
-								outputImage.setRGB(x, y, lum | (lum << 8) | (lum << 16));
-							}
-						}
-
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-
-					// Send the image
-
-					OutputStream outputStream = socket.getOutputStream();
-					// Create the byteArrayOutputStream
-					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-					// Convert BufferedImage to byteArrayOutputStream
-					ImageIO.write(outputImage, "jpg", byteArrayOutputStream);
-					// Convert size in byte[]
-					byte[] sizeOut = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
-					// Send size to server
-					outputStream.write(sizeOut);
-					// Send image to server
-					outputStream.write(byteArrayOutputStream.toByteArray());
-					outputStream.flush();
-
-
-				}
-				finally {
-					if (socket!=null) socket.close();
+			BufferedImage inputImage = ImageIO.read(new ByteArrayInputStream(imageAr));
+			outputImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(),BufferedImage.TYPE_INT_RGB);
+			for (int x = 0; x < inputImage.getWidth(); x++) {
+				for (int y = 0; y < inputImage.getHeight(); y++) {
+					int rgb = inputImage.getRGB(x, y);
+					int blue = 0x0000ff & rgb;
+					int green = 0x0000ff & (rgb >> 8);
+					int red = 0x0000ff & (rgb >> 16);
+					int lum = (int) (red * 0.299 + green * 0.587 + blue * 0.114);
+					outputImage.setRGB(x, y, lum | (lum << 8) | (lum << 16));
 				}
 			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		finally {
-			if (serverSocket != null) serverSocket.close();
-		}
+		
+		// Send the image
+		
+		OutputStream outputStream = socket.getOutputStream();
+		// Create the byteArrayOutputStream
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		// Convert BufferedImage to byteArrayOutputStream
+		ImageIO.write(outputImage, "jpg", byteArrayOutputStream);
+		// Convert size in byte[]
+		byte[] sizeOut = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
+		// Send size to server
+		outputStream.write(sizeOut);
+		// Send image to server
+		outputStream.write(byteArrayOutputStream.toByteArray());
+		outputStream.flush();
+		
+		System.out.println("Close: " + System.currentTimeMillis());
+		
+		System.out.println("close the socket");
+
+		serverSocket.close();
 
 	}
 
